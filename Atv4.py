db77bc4d6b2769b422745b4a0204e864c1c09bb4
JoãoG <J.Gomes0963@gmail.com>
2024-11-03 18:04:04 -0300

    Atualizando atividade

--- Atv4.py
+++ Atv4.py
@@ -1,3 +1,4 @@
+# ------------ receber numero e adicionar na lista -----------
 lista =[]
 j=True
 while j:
@@ -11,12 +12,33 @@ while j:
         else:
             print("digite um numero inteiro!!!")
 
-    r =str(input("- - - - - - - - - - - -\nQuer adicionar outro numero: \ns/n: "))
+    r =str(input("Quer adicionar outro numero: \ns/n: "))
     if r == "N" or r == "n":
         j=False
-# ------------ receber numero e adicionar na lista -----------
+print(lista)
 
 
+# Dada uma lista de números inteiros fornecida pelo usuário, escreva um programa 
+# que calcule e exiba a média dos valores que estão em índices ímpares. Imprima 
+# cada valor encontrado e, ao final, a média desses valores. 
+# Se não houver valores em índices ímpares, exiba uma mensagem adequada.
 
 
-print(lista) 
+media =0 
+h = 0
+c=0
+print("media de |", end="")
+while True:
+    
+    if h%2 == 0:
+        print(lista[h],end=" ")
+        media+=lista[h]
+        c+=1
+        
+    h+=1
+    
+    if h >= len(lista):
+        media=(media/c)
+        print("| e ",media)
+        break
+    
